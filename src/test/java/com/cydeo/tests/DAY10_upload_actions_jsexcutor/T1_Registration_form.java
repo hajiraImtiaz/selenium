f64ee0707cfe53d5b8a9a097eef63e6c2daabd01
hajiraImtiaz <hajiradani315@gmail.com>
2022-08-18 07:59:18 +0200

    day10

---
+++ src/test/java/com/cydeo/tests/DAY10_upload_actions_jsexcutor/T1_Registration_form.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.DAY10_upload_actions_jsexcutor;
+
+import com.cydeo.tests.utilities.ConfigurationReader;
+import com.cydeo.tests.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T1_Registration_form {
+
+    @Test
+    public void registration_form_test(){
+
+        //1. open browser
+        //2. Go to website: https://practice.cydeo.com/registration_form
+        //Driver.getDriver()--> driver.get(url)
+        Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
+
+        //3. Enter first name
+        //4. Enter last name
+        //5. Enter username
+        //6. Enter email address
+        //7. Enter password
+        //8. Enter phone number
+        //9. Select a gender from radio buttons
+        //10. Enter date of birth
+        //11. Select Department/Office
+        //12. Select Job Title
+        //13. Select programming language from checkboxes
+        //14. Click to sign up button
+        //15. Verify success message “You’ve successfully completed registration.” is
+        //displayed.
+
+    }
+
+}
